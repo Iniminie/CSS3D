98c8e8488b9ff1046873d303d6c42efe06ab5171
iniminie <fpaftw@gmail.com>
2012-02-25 01:37:28 +0000

    'v0.1'

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-/* Author:
+/* Author: Frank van Dongen
 
 */
+
+$(document).ready(function()
+{
+    var rotation = 0;
+    $(document).click(function()
+    {
+        rotation -= 90;
+        $('#container').css('-webkit-transform', 'rotateY('+rotation+'deg)'); 
+    });
+});
 
 
 
